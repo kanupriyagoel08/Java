b270687e1b36c2649496e084c761a4fd16cd1f1a
kanupriyagoel08 <kanupriyagoel.08@gmail.com>
2026-04-17 11:59:49 +0530

    Add files via upload

---
+++ streamapi5.java
@@ -0,0 +1,98 @@
+// import java.util.*;
+
+// public class streamapi5 {
+//     public static void main(String[] args) {
+
+//         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+//         int product = list.stream().reduce(1, (a, b) -> a * b);
+
+//         System.out.println("Product = " + product);
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class streamapi5 {
+//     public static void main(String[] args) {
+
+//         List<Integer> list = Arrays.asList(10, 25, 5, 40, 15);
+
+//         int max = list.stream().reduce((a, b) -> a > b ? a : b).get();
+
+//         System.out.println("Maximum = " + max);
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class streamapi5 {
+//     public static void main(String[] args) {
+
+//         List<Integer> list = Arrays.asList(10, 25, 5, 40, 15);
+
+//         int max = list.stream().reduce((a, b) -> a > b ? a : b).get();
+
+//         System.out.println("Maximum = " + max);
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class streamapi5 {
+//     public static void main(String[] args) {
+
+//         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+//         int sumEven = list.stream().filter(n -> n % 2 == 0).reduce(0, (a, b) -> a + b);
+
+//         System.out.println("Sum of even numbers = " + sumEven);
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class streamapi5 {
+//     public static void main(String[] args) {
+
+//         List<String> list = Arrays.asList("Java", "C", "Python", "Go", "Spring");
+
+//         int totalLength = list.stream().filter(s -> s.length() > 3).map(String::length).reduce(0, Integer::sum);
+
+//         System.out.println("Total length = " + totalLength);
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class streamapi5 {
+//     public static void main(String[] args) {
+
+//         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+//         int productOdd = list.stream().filter(n -> n % 2 != 0).reduce(1, (a, b) -> a * b);
+
+//         System.out.println("Product of odd numbers = " + productOdd);
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class streamapi5 {
+//     public static void main(String[] args) {
+
+//         List<Integer> list = Arrays.asList(5, -3, 10, -2, 7, -1);
+
+//         int sum = list.stream().map(n -> n < 0 ? 0 : n).reduce(0, (a, b) -> a + b);
+
+//         System.out.println("Sum = " + sum);
+//     }
+// }
+
+
